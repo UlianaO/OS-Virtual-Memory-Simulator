d2ff6415d7b0575f6e14c67ff371508153414907
Uliana Ozerova <ulyana.ozerova@gmail.com>
2022-10-13 22:42:46 -0400

    vms set up ready

--- vms.cpp
+++ vms.cpp
@@ -13,8 +13,8 @@ void vms()
     vector<PageEntry> LRU_SEC;
     int numReads = 0;
     int numWrites = 0;
-    int lru_size = (numOfFrames * p) / 100;
-    int fifo_size = numOfFrames - lru_size;
+    int lru_numOfFrames = (numOfFrames * p) / 100;
+    int fifo_numOfFrames = numOfFrames - lru_numOfFrames;
 
     cout << "VMS" << endl;
 
@@ -27,33 +27,45 @@ void vms()
         if (indexFound != FIFO_MAIN.end())
         {
             //update W bit            
-            if (inputVector[i].getOperation() == 'W')  {
+            if (inputVector[i].getOperation() == 'W')  
+            {
                 FIFO_MAIN[index].setOperation('W');
             }
         }
         //page is not in fifo 
         else 
         {    
+            auto indexFound = find(LRU_SEC.begin(), LRU_SEC.end(), inputVector[i]);
+            int index = distance(LRU_SEC.begin(), indexFound);
+            /*page is in LRU and not in FIFO; FIFO is full*/
+            if (indexFound != LRU_SEC.end()) {
+                //update W
+                //move to back to FIFO
+                //move front of fifo to lru making it the newest page
+            }
+
             /*page not in FIFO and FIFO not full*/
-            if (FIFO_MAIN.size() < numOfFrames) {
+            if (FIFO_MAIN.size() < numOfFrames) 
+            {
                 //add page to back of FIFO
                 FIFO_MAIN.push_back(inputVector[i]);
             }
+
+            /*page not in FIFO and FIFO is full, LRU not full*/
+            else if (FIFO_MAIN.size() == fifo_numOfFrames && LRU_SEC.size() < lru_numOfFrames) 
+            {
+                //eject from fifo
+                //move to lru, making it the newest
+            }
+
+            /*page is not in FIFO and not in LRU*/
+            else if (FIFO_MAIN.size() == fifo_numOfFrames && LRU_SEC.size() == lru_numOfFrames) 
+            {
+                //eject oldest lru
+                //move front of fifo to lru
+                //add new page to back of fifo
+            }
         }
-
-   /*page not in FIFO and FIFO is full, LRU not full*/
-   //eject from fifo
-   //move to lru, making it the newest
-
-   /*page is not in FIFO and not in LRU*/
-   //eject oldest lru
-   //move front of fifo to lru
-   //add new page to back of fifo
-
-       /*page is in LRU(Fifo full)*/
-   //update W
-   //move to back to FIFO
-   //move front of fifo to lru making it the newest page
     }
 
 
